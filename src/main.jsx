8bcfaedf8f6d56bb9f269afc3ca476045d70024b
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-08-28 22:39:30 +0530

    add:routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import Contact from "./components/Contact.jsx";
+import Error from "./components/Error.jsx";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+    errorElement: <Error />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>,
-)
+);
